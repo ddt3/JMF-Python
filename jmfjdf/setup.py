7808da7ec81438dd34aeb2ea01cd05817b8a51fb
Dries Dokter <dries.dokter@gmail.com>
2023-09-29 13:57:00 +0200

    First steps inactually creating a library

---
+++ jmfjdf/setup.py
@@ -0,0 +1,10 @@
+# from setuptools import find_packages, setup
+
+# setup(
+#     name='jmfjdf',
+#     packages=find_packages(include=['jmfjdf']),
+#     version='0.1.0',
+#     description='Python library for PRISMAsync jmfjdf queries and commands',
+#     author='Dries Dokter',
+#     install_requires=['requests', 'base64io', 'pathlib'],
+# )
